3ab597559aab2013ec10339a74c8a1b784afbf0f
Lahiru samaraweera <lahiru.studioxeon@gmail.com>
2019-08-18 07:49:58 +0530

    initial file setup

---
+++ tests/class.py
@@ -0,0 +1,15 @@
+class Developer():
+    
+    raise_amt = 23.3
+
+    def __init__(self, firstname, last_name, pay):
+        self.firstname = firstname
+        self.last_name = last_name
+        self.pay = p ay
+
+    @classmethod
+    def getByString(cls, string):
+        pass
+
+
+dev = Developer('lahiru', 'samaraweera', 12000)
